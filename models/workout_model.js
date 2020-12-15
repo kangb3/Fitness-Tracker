81b8eaa739462e7c1f9747435d5faf9455ba4acd
Baldeep Kang <bkang4612@gmail.com>
2020-12-14 18:43:42 -0800

    add worket_model for mongoose schema

---
+++ models/workout_model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now(),
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter exercise type",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter exercise name",
+        },
+        duration: {
+          type: Number,
+          required: "Enter workout duration",
+        },
+        distance: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
